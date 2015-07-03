c041abeb83286153b08bc47718d8d5e44998f0f7
Stefan Redfield <redfield.stefan@gmail.com>
2015-07-03 13:03:21 -0700

    add redirect function

--- app/js/controllers/main_controller.js
+++ app/js/controllers/main_controller.js
@@ -2,8 +2,14 @@
 
 module.exports = function(app) {
   app.controller('MainController', ['$scope', '$location', function($scope, $location) {
+    $scope.menuClicked = false;
 
-    $scope.menuClicked = false
+    $scope.menuOn = function() {
+      $scope.menuClicked = true;
+    };
 
+    $scope.redirect = function(destination) {
+      $location.path('/' + destination)
+    }
   }]);
 };
